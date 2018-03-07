852f2ec917a26cfa428754087b37f712887139cd
Zhanghanfhyc <35837755+Zhanghanfhyc@users.noreply.github.com>
2018-03-07 20:36:24 +0800

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map  {
+
+	public  Vector3 WorldToMap(Vector3 worldPos, Vector3 offset)
+    {
+        return worldPos - offset;
+    }
+    public Vector3 MapToWorld(Vector3 mapPos, Vector3 offset)
+    {
+        return mapPos + offset;
+    }
+}
