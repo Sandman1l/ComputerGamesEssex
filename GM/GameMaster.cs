badcf78c9c0c20bed133f745de70d87b8d92c8c4
Luis S. Flores <saloch_1s@hotmail.com>
2018-03-08 16:14:10 +0000

    Add files via upload

--- GM/GameMaster.cs
+++ GM/GameMaster.cs
@@ -21,35 +21,58 @@ public class GameMaster : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		
+
+
 	}
 
+
+
+
+
+
+	// MARK: INIT functions
 
 
 	//Doesn't take much to do this functions  but the Update Positions are escential for the movement Controller.
 	void PlayerPositionInit()
 	{
 		List<GameObject> pl = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Character"));
-		foreach (GameObject pieze in pl) 
+		foreach (GameObject pieze in pl)
 		{
 			playerChar.Add(pieze.GetComponent<Transform>().position);
 		}
 	}
+
 	void ObstaclesInit()
 	{
 		List<GameObject> enem = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Enemy"));
-		foreach (GameObject pieze in enem) 
+		foreach (GameObject pieze in enem)
 		{
 			enemies.Add(pieze.GetComponent<Transform>().position);
 		}
 		//for actual tag == obstacle
 		/*List<GameObject> enem = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Enemy"));
-		foreach (GameObject pieze in enem) 
+		foreach (GameObject pieze in enem)
 		{
 			obstacles.Add(pieze.GetComponent<Transform>().position);
 		}*/
 
+	}
+//The initialization of the player and enemy pieces NEED to happen AFTER their coordinates have been taken
+// otherwise there will be problems with the movement manager.
+	void initAllPlEne()
+	{
 
+		List<GameObject> pl = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Character"));
+		foreach (GameObject pieze in pl)
+		{
+			pieze.GetComponent<CharController>().initPlEne();
+		}
+		List<GameObject> enem = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Enemy"));
+		foreach (GameObject pieze in enem)
+		{
+			pieze.GetComponent<CharController>().initPlEne();
+		}
 	}
 
 
